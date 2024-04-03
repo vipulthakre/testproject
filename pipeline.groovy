6cecea9b3f76577c273f8033d4f477c012cc3d3c
vipulthakre <vipulthakre199@gmail.com>
2024-04-03 21:38:34 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ def sast(){
 }
 def build(){
   sh "chmod +x -R ${env.WORKSPACE}"
-  sh "/var/lib/jenkins/workspace/${workspaceName}/build_app.sh ${workspaceName}
+  sh "/var/lib/jenkins/workspace/${workspaceName}/build_app.sh ${workspaceName}"
 }
 def uploadArtfactInJfrog(){
   echo "Jfrog Stage"
