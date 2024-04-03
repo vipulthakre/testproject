43d61d906ec9a5f987ae77abbabde9ec546823e0
vipulthakre <vipulthakre199@gmail.com>
2024-04-03 21:34:17 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,10 @@
+def sast(){
+  echo "Checkmarx stage"
+}
+def build(){
+  sh "chmod +x -R ${env.WORKSPACE}"
+  sh "/var/lib/jenkins/workspace/${workspaceName}/build_app.sh ${workspaceName}
+}
+def uploadArtfactInJfrog(){
+  echo "Jfrog Stage"
+}
